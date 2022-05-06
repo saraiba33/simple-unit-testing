10d36b3c379496f334a3026dd0c23af3e42e91ef
Sara Ibarra <saraiba33@gmail.com>
2022-05-06 12:51:26 -0600

    Add test 3 solution and refactor js file 3

--- src/3.js
+++ src/3.js
@@ -1,41 +1,38 @@
-var mentors = [
-    {
-        name: 'Irina',
-        title: 'Dr.',
-        yearsOfExperience: 10
-    }, {
-        name: 'Ashleigh',
-        title: 'Dame',
-        yearsOfExperience: 20
-    }, {
-        name: 'Etza',
-        title: 'Professor',
-        yearsOfExperience: 30
-    }
-];
+const mentors = [{
+    name: 'Irina',
+    title: 'Dr.',
+    yearsOfExp: 10
+}, {
+    name: 'Ashleigh',
+    title: 'Dame',
+
+    yearsOfExp: 20
+}, {
+    name: 'Etza',
+    title: 'Professor',
+    yearsOfExp: 30
+}];
 
 function getMentorInfo(mentors, name) {
-    var result;
-    var greeting = 'Hello ';
-
-    result = mentors.find(function (person) {
+    let result = mentors.find(function(person) {
         return person.name === name;
     });
-
-    var mentorTitle = 'Junior Developer';
-
-    if (result.yearsOfExp > 10) {
+    let mentorTitle = '';
+    if (result.yearsOfExp > 20) {
+        mentorTitle = 'Very Senior Developer'
+    } else if (result.yearsOfExp <= 10) {
+        mentorTitle = 'Junior Developer';
+    } else if (result.yearsOfExp > 10) {
         mentorTitle = 'Senior Developer';
-    } else if (result.yearsOfExp > 20) {
-        mentorTitle = 'Very Senior Developer';
     }
-
     result.jobTitle = mentorTitle;
-    result.fullName = result.title + ' ' + result.name;
+    result.fullName = `${result.title} ${result.name}`
 
     return result;
 }
 
-var result = getMentorInfo(mentors, 'Etza')
 
-module.exports = getMentorInfo
+module.exports = {
+    getMentorInfo,
+    mentors
+}
